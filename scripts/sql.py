615794751cba2e83b231ad88f4206ae953805e5f
Rudy Corona <rcorona@utexas.edu>
2016-04-17 18:11:20 -0500

    Added first version of sql script, need to test table building.

--- scripts/sql.py
+++ scripts/sql.py
@@ -1,85 +1,101 @@
 #!/usr/bin/python
 
 import MySQLdb
+import sys
 
-host = "localhost"
-user = "root"
-passwd = "nlp"
+#Deletes database for rebuilding. 
+def clean():
+    sql = "DROP DATABASE IF EXISTS NLP_VECTORS"
 
-#Opens database connection. 
-db = MySQLdb.connect(host, user, passwd)
-
-#Cursor object used to interact with MySQL
-cursor = db.cursor()
-
-#Creates database if it doesn't exist. 
-sql = "CREATE DATABASE IF NOT EXISTS TESTDB"
-
-cursor.execute(sql)
-
-#Selects database.
-sql = "USE TESTDB"
-
-cursor.execute(sql)
-
-#Creates table if doesn't exist yet. 
-sql = """CREATE TABLE IF NOT EXISTS EMPLOYEE (
-         FIRST_NAME CHAR(20) NOT NULL,
-         LAST_NAME CHAR(20),
-         AGE INT,
-         SEX CHAR(1),
-         INCOME FLOAT )"""
-
-cursor.execute(sql)
-
-#Inserts element into table. 
-sql = """INSERT INTO EMPLOYEE (FIRST_NAME,
-                               LAST_NAME,
-                               AGE,
-                               SEX,
-                               INCOME)
-                   VALUES('%s', '%s', '%d', '%c', '%d')""" % \
-                         ('Mac', 'Mohan', 20, 'M', 2000)
-
-try:
     cursor.execute(sql)
-
-    #Commit changes to database. 
+    
+    #Commits changes. 
     db.commit()
 
-except:
-    #Rollback in case of error. 
-    db.rollback()
 
-    print "Error, had to rollback!"
+#Creates database. 
+def create_db():
+    #Cleans before building. 
+    clean()
 
-#Reads from database using condition. 
-sql = "SELECT * FROM EMPLOYEE WHERE INCOME > '%d'" % (1000)
+    #Creates database if it doesn't exist. 
+    sql = "CREATE DATABASE IF NOT EXISTS NLP_VECTORS"
 
-cursor.execute(sql)
+    cursor.execute(sql)
 
-#Fetch all the rows in a list of lists. 
-results = cursor.fetchall()
+    #Selects database.
+    sql = "USE NLP_VECTORS"
 
-for row in results:
-    fname = row[0]
-    lname = row[1]
-    age = row[2]
-    sex = row[3]
-    income = row[4]
+    cursor.execute(sql)
 
-    #prints result
-    print "fname = %s, lname = %s, age = %d, sex = %c, income = %d" % \
-           (fname, lname, age, sex, income)
+    #Creates vector tables if doesn't exist yet. 
+    sql = """CREATE TABLE IF NOT EXISTS WORD2VEC (
+             WORD VARCHAR(%d) NOT NULL,
+             VECTOR MEDIUMTEXT NOT NULL )""" % (max_word_len)
 
-sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+    cursor.execute(sql)
 
-cursor.execute(sql)
-db.commit()
+    sql = """CREATE TABLE IF NOT EXISTS GLOVE (
+             WORD VARCHAR(%d) NOT NULL, 
+             VECTOR MEDIUMTEXT NOT NULL )""" % (max_word_len)
 
-sql = "DELETE FROM EMPLOYEE WHERE AGE > '%d'" % (20)
+    cursor.execute(sql)
 
-cursor.execute(sql)
-db.commit()
+    #Commits changes. 
+    db.commit()
 
-db.close()
+def build_table(table_name, file_name):
+    if not table_name == "GLOVE" or table_name == "WORD2VEC":
+        print "Wrong table name used! Try GLOVE or WORD2VEC silly."
+    
+    #Adds each vector to table. 
+    for line in file_name:
+        word, vector = line.split()
+
+        sql = """INSERT INTO %s (WORD, VECTOR) 
+                 VALUES('%s', '%s)""" % (word, vector)
+
+        cursor.execute(sql)
+
+    #Orders table in alphabetical order by word. 
+    sql = """ALTER TABLE %s ORDER BY WORD"""
+    cursor.execute(sql)
+
+    #Commits changes. 
+    db.commit()
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = "root"
+    passwd = "julian"
+
+    #Variables for table building. 
+    max_word_length = 250
+
+    #Opens database connection. 
+    db = MySQLdb.connect(host, user, passwd)
+
+    #Cursor object used to interact with MySQL
+    cursor = db.cursor()
+
+    #Ensures argument length correctness. 
+    if len(sys.argv < 2):
+        print "Need at least one argument!"
+
+    #Command to execute in db. 
+    command = sys.argv[1]
+
+    #Different command cases. 
+    if command == "clean":
+        clean()
+    elif command == "create_db":
+        create_db()
+    elif command == "build_table":
+        #Ensures arguments are all there. 
+        if not len(sys.argv == 4):
+            print "Expecting 4 arguments!"
+            print "Usage: $python sql.py build_table [table name] [word vector file name]"
+        else:
+            build_table(sys.argv[2], sys.argv[3])
+
+    db.close()
