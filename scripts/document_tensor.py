d0efd5a71a9cfafea55ec74eddd6ad16e38119d9
siajat <siajat@utexas.edu>
2016-04-18 19:36:38 -0700

    wrote most of doc tensor, need edits to invert index

---
+++ scripts/document_tensor.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class DocumentTensor:
+    def __init(self, name):
+        #File prefix name before extension. 
+        self.name = name
+
+        #Dimmensions for tensor. 
+        self.num_sent = None
+        self.max_sent_len = None
+        self.word_dimmension = None
+
+        #The tensor. 
+        self.tensor = None
+
+    def build(self, serialized_inverted_index_name):
+        #Load pickled inverted index object.. 
+        inverted_index = pickle.load(open(serialized_inverted_index_name, 'rb'))
+
+        #Gets document inverted index dictionary as well as list of statistics for document. 
+        word2sent, stats = inverted_index.get_doc_data(self.name)
+
+        #Gets dimmensions for tensor and initializes it. 
+        #TODO Get dimmensions from stats (also implement stats)
+        self.tensor = np.zeros((self.num_sent, self.max_sent_len, self.word_dimmension))
+
+        #Builds tensor by iterating over every word and every sentence. 
+        for word in word2sent:
+
+            sent2word = word2sent[word]
+
+            for sent in sent2word:
+                #Get list of sentence indeces where word appears. 
+                word_index_list = sent2word[sent]
+
+                #Place word vector where necessary in sentence matrix. 
+                for word_index in word_index_list:
+                    self.tensor[sent, word_index, :] = #TODO get numpy vector from db. 
+
--- scripts/inverted_index.py
+++ scripts/inverted_index.py
@@ -19,37 +19,37 @@ class InvertedIndex:
     where each tuple maps from a sentence index 
     '''
     def __init__(self):
-        self.word_2_docSentList = {}
+        self.doc2word = {}
         
 
-    def insert(self, word, doc, sent_num, word_pos):
-        # this word has been seen before
-        if word in self.word_2_docSentList:
-            doc2sent_num = self.word_2_docSentList[word]
+    def insert(self, word, doc, sent, word_pos):
+        # this document has been seen before
+        if doc in self.doc2word:
+            word2sent = self.doc2word[doc]
             
-            # this word in this document has been seen before
-            if doc in doc2sent_num:
-                sent_num2word  = doc2sent_num[doc]
+            # this document containing this word has been seen before
+            if word in word2sent:
+                sent2word_ind = word2sent[word]
                 
-                # this word has been seen in this document in this same sentence
-                if sent_num in sent2word:
-                    word_indices = sent_num2word[sent_num]
+                # this document containing this word has seen this sentence before and this word is in this sentence 
+                if sent in sent2word_ind:
+                    word_indices = sent2word_ind[sent]
                     word_indices.append(word_pos)
-                    sent_num2word[sent_num] = word_indices
+                    sent2word_ind[sent] = word_indices
                 
                 # this word in this document has been seen in a different sentence
                 else:
-                    sent_num2word[sent_num] = [word_pos]
+                    sent2word_ind[sent] = [word_pos]
 
             # this word has not been seen in this document before
             else:
-                doc2sent_num[doc] = {sent : [word_pos]}
+                word2sent[word] = {sent : [word_pos]}
             
-        # first time the word has been seen in the corpus
+        # first time the document  has been seen in the corpus
         else:
-            sent2word = {sentence_num: [word_pos]}
-            doc2sent = {doc: sent2word}
-            self.word_2_docSentList[word] = doc2sent
+            sent2word_ind = {sent: [word_pos]}
+            word2sent = {word: sent2word_ind}
+            self.doc2word[doc] = word2sent
             
     
     def build_representation(self, curr_dir):
@@ -87,7 +87,7 @@ class InvertedIndex:
                 # segment the sentence 
                 sent_segm = sent_tokenize(curr_file_string)
                 num_sentences = len(sent_segm)
-                max_sent_len = len_longest_sentence(curr_file_string)
+                max_sent_len = self.len_longest_sentence(curr_file_string)
          
                 # tokenize each sentence
                 sent_counter = 0
@@ -96,13 +96,13 @@ class InvertedIndex:
                     
                     tok_counter = 0 
                     for tok in tok_segm:   
-                        self.insert(word, doc_name, sent_counter, tok_counter)
+                        self.insert(tok, doc_name, sent_counter, tok_counter)
                         tok_counter += 1    
                     
                     sent_counter += 1
-                
-                #pickle.dump(save_arr, open(old_dir + '/' + doc_name + '_npArr.p','wb'))
-    
+                 
+        # change back to the original working directory
+        os.chdir(old_dir)    
     
     def len_longest_sentence(self, curr_file_string):
         sent_segm = sent_tokenize(curr_file_string)
@@ -115,8 +115,8 @@ class InvertedIndex:
         return longest
 
     
-    def serialize_index(self, save_name):
-        pickle.dump(self.word_2_docSentList, open(save_name + ".p", 'wb'))
+    def serialize_inverted_index(self, save_name):
+        pickle.dump(self.doc2word, open(save_name + ".p", 'wb'))
 
 
 
